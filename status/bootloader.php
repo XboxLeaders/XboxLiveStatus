d85ae01023785ac98d383ace9834a439005dc0a6
Jason Clemons <me@jasonclemons.me>
2013-07-24 16:58:44 -0400

    OAuth

--- status/bootloader.php
+++ status/bootloader.php
@@ -11,13 +11,34 @@
  *******************************************************************************/
 
 require('classes/xboxstatus.class.php');
+require('oauth/twitteroauth.php');
 
+/*!
+ * Twitter Consumer Key
+ * This must be obtained from http://dev.twitter.com
+ */
 define('CONSUMER_KEY', 'PlaceYourTwitterConsumerKeyHere');
 
+/*!
+ * Twitter Consumer Secret
+ * This must be obtained from http://dev.twitter.com
+ */
 define('CONSUMER_SECRET', 'PlaceYourTwitterConsumerSecretHere');
 
+/*!
+ * Twitter Access Key
+ * This must be obtained from http://dev.twitter.com
+ */
 define('ACCESS_KEY', 'PlaceYourTwitterAccessKeyHere');
 
+/*!
+ * Twitter Access Secret
+ * This must be obtained from http://dev.twitter.com
+ */
 define('ACCESS_SECRET', 'PlaceYourTwitterAccessSecretHere');
 
+/*!
+ * Custom error logging
+ * This will log errors in a custom directory. Change to 'false' to disable.
+ */
 define('ERROR_LOG', '/full/path/to/error.log');
